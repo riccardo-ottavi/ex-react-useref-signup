3411196a8b833948156377dfd8c57e40706d292f
riccardo-ottavi <ottavi.ps@libero.it>
2026-01-12 13:00:06 +0100

    inizializza gli stati

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [specialization, setSpecialization] = ("");
+  const [experience, setExperience] = (null);
+  const [description, setDescription] = ("")
 
   return (
     <>
       <h1>Prova</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" name="" id="" />
+        <input type="text" name="" id="" />
+        <input type="password" name="" id="" />
+        <input type="select" />
+        <input type="number" />
+        <textarea name="" id=""></textarea>
+      </form>
     </>
   )
 }
